9cbf1c3d6d84fc2650289749c968d08da60f0fd5
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-03-07 10:07:42 +0530

    Binary search

---
+++ binary-search.js
@@ -0,0 +1,15 @@
+function binarySearch(array, element) {
+  let subArray = [];
+  const midIndex = Math.floor(array.length / 2);
+  if (array[midIndex] === element) {
+    return true;
+  }
+  if (array[midIndex] > element) {
+    subArray = array.slice(0, midIndex);
+  } else {
+    subArray = array.slice(midIndex + 1);
+  }
+
+  return binarySearch(subArray, element);
+}
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
