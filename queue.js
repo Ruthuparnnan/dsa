aadb8cb776435bb904ed7164d8b83c724ace860b
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-03-20 17:55:54 +0530

    queue

---
+++ queue.js
@@ -0,0 +1,39 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[0];
+    }
+    return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const nifty = new Queue();
+nifty.enqueue("bullish");
+nifty.dequeue();
+nifty.isEmpty();
+
+nifty.print()
