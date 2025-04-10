93ddcd03c03ef5cae191b0fc9f741af0c5066290
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-04-10 11:29:49 +0530

    graph add delete print

--- graph.js
+++ graph.js
@@ -4,7 +4,7 @@ class Graph {
   }
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
+      this.adjacencyList[vertex] = new Set();
     }
   }
   addEdge(vertex1, vertex2) {
@@ -14,7 +14,55 @@ class Graph {
     if (!this.adjacencyList[vertex2]) {
       this.addVertex(vertex2);
     }
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
+
+  print() {
+    for (let vertex in this.adjacencyList) {
+      console.log(
+        vertex + " -> " + Array.from(this.adjacencyList[vertex])?.join(", ")
+      );
+    }
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) {
+      this.adjacencyList[vertex1].delete(vertex2);
+    }
+    if (this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex2].delete(vertex1);
+    }
+  }
+
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      for (let adjacentVertex of this.adjacencyList[vertex]) {
+        this.removeEdge(vertex, adjacentVertex);
+      }
+      delete this.adjacencyList[vertex];
+    }
+  }
+
+  getVertices() {
+    return Object.keys(this.adjacencyList);
+  }
+  getEdges() {
+    const edges = [];
+    for (let vertex in this.adjacencyList) {
+      for (let adjacentVertex of this.adjacencyList[vertex]) {
+        edges.push([vertex, adjacentVertex]);
+      }
+    }
+    console.log(edges);
   }
 }
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.removeEdge("A", "B");
+graph.print();
+console.log("Vertices:", graph.getVertices());
