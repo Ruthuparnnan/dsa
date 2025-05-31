19ac1474ff183e9122706efa6012ba01950bccaa
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-05-31 09:04:47 +0530

    most common word

---
+++ leetcode/most-common-word.js
@@ -0,0 +1,32 @@
+function mostCommonWord(paragraph, banned) {
+  const cleanedParagraphArray = paragraph
+    .toLowerCase()
+    .replace(/[^a-z\s]/g, "")
+    .split(/\s+/);
+  const bannedSet = new Set(banned);
+  const countMap = new Map();
+
+  for (let word of cleanedParagraphArray) {
+    if (!bannedSet.has(word)) {
+      countMap.set(word, (countMap.get(word) || 0) + 1);
+    }
+  }
+
+  let maxCount = 0;
+  let maxWord = "";
+
+  for (let [word, count] of countMap) {
+    if (count > maxCount) {
+      maxCount = count;
+      maxWord = word;
+    }
+  }
+
+  return maxWord;
+}
+
+console.log(
+  mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", [
+    "hit",
+  ])
+);
