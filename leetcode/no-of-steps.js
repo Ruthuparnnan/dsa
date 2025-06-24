faa4b69c90f088c9c096334cdf4f368237ea2055
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-06-24 13:47:09 +0530

    add function to calculate the number of steps to reduce a number to zero

---
+++ leetcode/no-of-steps.js
@@ -0,0 +1,14 @@
+function noOfSteps(num) {
+  let count = 0;
+
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    count++;
+  }
+
+  return count;
+}
