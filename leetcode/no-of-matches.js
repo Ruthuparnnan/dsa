6df41468ffbd0bb1a62c28f93087a0b3f458d321
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-08-01 05:11:55 +0530

    number of matches

---
+++ leetcode/no-of-matches.js
@@ -0,0 +1,14 @@
+/**
+* @param {number} n
+* @return {number}
+*/
+var numberOfMatches = function(n) {
+    let sum=0
+ 
+    while(n>1){
+        sum += Math.floor(n/2);
+        n = Math.round(n/2)
+    }
+ 
+    return sum
+};
