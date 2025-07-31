e74a7e723dd83bfd6c6152031188cc8366facee9
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-08-01 05:12:52 +0530

    count students

---
+++ leetcode/count-students.js
@@ -0,0 +1,21 @@
+/**
+* @param {number[]} students
+* @param {number[]} sandwiches
+* @return {number}
+*/
+var countStudents = function(students, sandwiches) {
+    let count=0;
+ 
+    while(students.length && count < students.length){
+        if(students[0] === sandwiches[0]){
+            students.shift()
+            sandwiches.shift()
+            count=0
+        }else{
+            students.push(students.shift());
+            count++
+        }
+    }
+ 
+    return students.length
+};
