4b115aa730f7d00aa387806730dd3a69d4cb3729
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-08-20 08:13:08 +0530

    array concatenation

---
+++ leetcode/array-concatenation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  const ans = [];
+
+  for (let i = 0; i < nums.length * 2; i++) {
+    if (i >= nums.length) {
+      ans[i] = nums[i % nums.length];
+    } else {
+      ans[i] = nums[i];
+    }
+  }
+
+  return ans;
+};
