34315658dba35c50c4fedf04188b927143aaa528
Ruthuparnnan <ruthuparnnan@dataguardnxt.com>
2025-09-22 10:26:43 +0530

    implement uniquePaths function to calculate the number of unique paths in a grid

---
+++ leetcode/medium/unique-path-dp.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  // Create a 2D dp array filled with 1s
+  let dp = Array.from({ length: m }, () => Array(n).fill(1));
+
+  // Fill the dp table
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
