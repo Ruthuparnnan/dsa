bfae0209ba7b18fda62b238a3ed8f5cea06e456c
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-10-24 06:31:16 +0530

    connect bst

---
+++ leetcode/medium/connect-bst-level__116.js
@@ -0,0 +1,37 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+  if (!root) return null;
+
+  let leftmost = root;
+
+  while (leftmost.left) {
+    let head = leftmost;
+
+    while (head) {
+      head.left.next = head.right;
+
+      if (head.next) {
+        head.right.next = head.next.left;
+      }
+
+      head = head.next;
+    }
+
+    leftmost = leftmost.left;
+  }
+
+  return root;
+};
