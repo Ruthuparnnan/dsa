c512d8b58ec44ce5ab90998ff459f532e78c190f
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-11-30 06:42:24 +0530

    implement numIslands function to count the number of islands in a grid

---
+++ leetcode/medium/number-of-islands.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  let count = 0;
+
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === "0") return;
+
+    grid[r][c] = "0";
+
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === "1") {
+        count++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return count;
+};
