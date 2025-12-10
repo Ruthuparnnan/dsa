14acd37ccea8a96bb2e28c8974b0e01b26e19ede
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-12-10 06:20:46 +0530

    implement majorityElement function to find all elements that appear more than n/3 times in an array

---
+++ leetcode/medium/majority-element-2__229.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+    if(nums.length < 2)return nums;
+
+    let candidate1 , candidate2;
+    let count1=0,count2=0;
+    const result =[];
+
+    for(let num of nums){
+        if(num === candidate1)count1++;
+        else if(num === candidate2)count2++;
+        else if(count1 === 0){
+            candidate1=num;
+            count1++;
+        }
+        else if(count2 === 0){
+            candidate2=num;
+            count2++;
+        }else{
+            count1--;
+            count2--;
+        }
+    }
+
+     count1 = 0;
+    count2 = 0;
+
+    for (let num of nums) {
+        if (num === candidate1) count1++;
+        else if (num === candidate2) count2++;
+    }
+
+    if(count1 > (nums.length /3))result.push(candidate1)
+    if(count2 > (nums.length /3))result.push(candidate2)
+    return result;
+};
