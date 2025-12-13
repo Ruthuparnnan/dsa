45ab70ba3456670861654b84a231863223d8c603
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-12-13 06:48:29 +0530

    implement lowestCommonAncestor function to find the lowest common ancestor of two nodes in a binary tree

---
+++ leetcode/medium/lca-of-bt__236.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  const dfs = (node) => {
+    if (node === null || node === p || node === q) return node;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    if (left && right) return node;
+
+    return left ? left : right;
+  };
+
+  return dfs(root);
+};
