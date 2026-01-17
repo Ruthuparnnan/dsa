76cee913716998ad69ae7e42d85bb2cd98979ef5
Ruthuparnnan <ruthuparnnan123@gmail.com>
2026-01-17 07:01:33 +0530

    implement increasingTriplet function to determine if there exists an increasing triplet subsequence in an array

---
+++ leetcode/medium/increasing-triplet-sequence__334.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function (nums) {
+  let first = Number.POSITIVE_INFINITY;
+  let second = Number.POSITIVE_INFINITY;
+
+  for (const num of nums) {
+    if (num <= first) first = num;
+    else if (num <= second) second = num;
+    else return true;
+  }
+
+  return false;
+};
