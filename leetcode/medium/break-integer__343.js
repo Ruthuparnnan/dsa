0853763b5409869ee9b4fddfcd03232dca766219
Ruthuparnnan <ruthuparnnan123@gmail.com>
2026-01-20 06:01:31 +0530

    implement integerBreak function to maximize the product of integers that sum to n

---
+++ leetcode/medium/break-integer__343.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function (n) {
+  if (n <= 3) return n - 1;
+
+  let result = 1;
+
+  while (n > 4) {
+    result *= 3;
+    n -= 3;
+  }
+
+  return n * result;
+};
