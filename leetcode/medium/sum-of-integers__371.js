39077278a02c05acb1c9d8e1fbe57328ac835718
Ruthuparnnan <ruthuparnnan123@gmail.com>
2026-01-28 05:51:53 +0530

    implement getSum function to calculate the sum of two integers without using arithmetic operators

---
+++ leetcode/medium/sum-of-integers__371.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while(b !== 0){
+        const sum = a ^ b;
+        const carry = (a & b) << 1
+
+        a = sum;
+        b = carry;
+    }
+
+    return a;
+};
