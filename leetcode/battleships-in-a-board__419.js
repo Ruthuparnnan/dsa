88a661ce0b50194f0a59548bd898686810712a47
Ruthuparnnan <ruthuparnnan123@gmail.com>
2026-03-04 05:52:29 +0530

    add countBattleships function to count the number of battleships on a board

---
+++ leetcode/battleships-in-a-board__419.js
@@ -0,0 +1,23 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+    let count =0;
+
+    const m = board.length;
+    const n = board[0].length;
+
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(board[i][j] === 'X'){
+                if(i>0&&board[i-1][j]==='X')continue;
+                if(j>0&&board[i][j-1]==='X')continue;
+                
+                count++
+            }
+        }
+    }
+
+    return count;
+};
